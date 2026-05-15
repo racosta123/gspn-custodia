2ac9a489aae4d986cae0973b288b74a5642a9206
racosta123 <acosta4770@gmail.com>
2026-05-14 22:40:35 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,11 @@
-const CACHE = 'gspn-v2';
-const ASSETS = ['./', './index.html', './supervisor.html', './manifest.json', './icon-192.png', './icon-512.png'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
+// Service worker minimo - sin cache, siempre red
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+  );
   self.clients.claim();
 });
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html'))));
+  e.respondWith(fetch(e.request));
 });
